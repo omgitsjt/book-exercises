bc8a7f773426afc76fabfa12b7d7509a23737e89
Jonathan J Thomas <omgitsjt@uw.edu>
2020-01-31 16:20:08 -0800

    Week 4 Exercises

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -15,30 +15,36 @@ View(grants)
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
 
-
-
+organization <- as.vector(grants$organization)
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
 
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
 
+print(mean(grants$total_amount, na.rm = TRUE))
 
 # What was the dollar amount of the largest grant?
 
+print(max(grants$total_amount))
 
 # What was the dollar amount of the smallest grant?
 
+print(min(grants$total_amount))
 
 # Which organization received the largest grant?
 
+print(grants[grants$total_amount == max(grants$total_amount), "organization"])
 
 # Which organization received the smallest grant?
 
+print(grants[grants$total_amount == min(grants$total_amount), "organization"])
 
 # How many grants were awarded in 2010?
 
+print(nrow(grants[grants$start_year == 2010, ]))
--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -3,6 +3,9 @@
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
 
+install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
@@ -10,15 +13,21 @@
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
 
+View(flights)
+nrow(flights)
+ncol(flights)
+colnames(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
 
+mutate(flights, delayed_in_air == arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
 
+flights <- arrange(flights, -delayed_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
@@ -30,13 +39,19 @@
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
 
+summarize(flights, avg = mean(delayed_in_air, na.rm = TRUE))
+# Flights gained time
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
 
+seatac <- filter(flights, dest == "SEA")
+seatac <- select(seatac, origin, dest, delayed_in_air)
 
 # On average, did flights to SeaTac gain or loose time?
 
+summarize(seatac, avd_delayed = mean(delayed_in_air, na.rm = TRUE))
+# Flights lost time
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
